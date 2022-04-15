c590343b0e97da0427f8c7cc5a02e285a9706206
rutuja <rutujaharidas1996@gmail.com>
2022-04-15 13:21:16 -0700

    Added APP files

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {View} from 'react-native'
+import Expo from 'expo'
+import {Scene, Mesh, MeshBasicMaterial, PerspectiveCamera, BoxBufferGeometry} from 'three'
+import ExpoTHREE, {Renderer} from 'expo-three'
+import {ExpoWebGLRenderingContext, GLView} from 'expo-gl'
 
-export default function App() {
+const App = ()=>{
+
+  const onContextCreate = async (gl)=> {
+    const scene = new Scene();
+
+    const Camera = new PerspectiveCamera(75,
+      gl.drawingBufferWidth/gl.drawingBufferHeight, 0.1, 1000
+      )
+      
+      Camera.position.z = 2
+      
+      const renderer = new Renderer({gl})
+      renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight)
+
+      const geometry = new BoxBufferGeometry(1,1,1)
+      const material = new MeshBasicMaterial({
+        color: 'red'
+      })
+      const cube = new Mesh(geometry, material)
+      scene.add(cube)
+
+      const render = ()=> {
+        requestAnimationFrame(render)
+        cube.rotation.x += 0.01
+        cube.rotation.y += 0.01
+        renderer.render(scene, Camera)
+        gl.endFrameEXP()
+      }
+
+      render()
+      
+    }   
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <View>
+      <GLView      
+        onContextCreate = {onContextCreate}
+        style = {{
+          width:500,
+          height: 500
+        }}
+      />
+      </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
